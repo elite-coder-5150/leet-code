3a89c8fc0dbe036ce6cab145ed18e0314d9add65
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-12 22:37:03 -0600

    added a few comments explaining the algorithm

--- easy/invert-binary-tree.js
+++ easy/invert-binary-tree.js
@@ -1,10 +1,36 @@
 // Given the root of a binary tree, invert the tree, and return its root.
+
 const invertTree = (root) => {
     if (!root) return null;
     [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
     return root;
 };
-
+/**
+ * the above function does the following
+ *      1. it check to see if root has no value, if so simple return null.
+ *      2. it inverts the tree by calling itself by switching the right for the leff.
+ *      3. simply return the root
+ *  
+ * @param {*} root 
+ * @returns 
+ */
+/**
+ * this function does the following:
+ *      1. it checks the value of root, if root has no value then return null
+ *      2. else it
+ *          1. temp is set to root.left
+ *          2. root.left = invertTree(root.right);
+ *          3. root.right = invertTree(temp);
+ *          4. call invertTree on root.right);
+ *          5. then call InvertTree on root.left
+ *          6. simple return the root.
+ * 
+ *      3. I can write the same algorithm in a variety of languages and frameworks not just angular.
+ *      
+ * 
+ * function methodology:
+ *      recursion 
+ */
 // best solution
 const invert_tree = (root) => {
     if (!root) {
@@ -19,3 +45,4 @@ const invert_tree = (root) => {
 
         return root;
     }
+}
