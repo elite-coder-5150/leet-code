fa3c8c7de79c253de419d648ad040d6f388d56fb
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-22 10:04:10 -0600

    detech weither a list has a cycle or not

--- easy/list-has-cycle.js
+++ easy/list-has-cycle.js
@@ -1,9 +1,9 @@
 /**
  * Given head, the head of a linked list, determine if the linked list has a cycle in it.
-
-There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
-
-Return true if there is a cycle in the linked list. Otherwise, return false.
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by 
+ * continuously following the next pointer. Internally, pos is used to denote the index of the node 
+ * that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
  */
 const hasCycle = (head) => {
     let slow = head;
@@ -15,3 +15,22 @@ const hasCycle = (head) => {
     }
     return false;
 }
+
+class solution {
+    hasCycle(head) {
+        let slow = fast = head
+
+        while (fast && fast.next) {
+            slow = slow.next
+            fast = fast.next.next
+
+            if (slow === fast)
+                return true
+        }
+    }
+}
+
+/**
+ * if the two pointer are equal in the code above, that indicates that there is a cycle 
+ * within the list. And if the while loop is complete the function returns false.
+ */
