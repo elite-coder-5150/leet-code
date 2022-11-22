3e80b1456cc7b1c1ff27cb862d3739c99672b7a5
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-22 10:05:25 -0600

    the longest common prefix algorithm, solution #38

--- easy/longestCommonPrefix.js
+++ easy/longestCommonPrefix.js
@@ -22,3 +22,40 @@ const longestCommonPrefix = (strs) => {
     }
     return prefix;
 };
+
+//! This is what makes VSCode batter then webstorm. I can define function my own way
+//! Webstorm would have a fit with this code. :)
+// TODO: 
+class solution {
+    longestCommonPrefix(strs) {
+        if (strs.length === 0)
+            return '';
+
+        let prefix = strs[0];
+
+        for (let i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) !== 0) {
+                prefix = prefix.substring(0, prefix.length - 1);
+
+                if (prefix === '') {
+                    return '';
+                }
+            }
+        }
+
+        return prefix;
+    };
+
+    anotherAlgorithm(node) {
+
+    }
+}
+
+let sol = new solution();
+
+// and the code completion works as well. So i defined the function properly
+sol.longestCommonPrefix();
+sol.anotherAlgorithm(null);
+/**
+ * underlying data structure array and strings
+ */
